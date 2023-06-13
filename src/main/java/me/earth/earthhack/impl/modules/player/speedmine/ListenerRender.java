5c4410fec82a8ead569c6d2ef856a45ec87e5982
Kg <88619756+KgDW@users.noreply.github.com>
2023-06-13 22:18:04 +0800

    Easing

--- src/main/java/me/earth/earthhack/impl/modules/player/speedmine/ListenerRender.java
+++ src/main/java/me/earth/earthhack/impl/modules/player/speedmine/ListenerRender.java
@@ -8,11 +8,22 @@ import me.earth.earthhack.impl.util.render.Interpolation;
 import net.minecraft.util.math.AxisAlignedBB;
 import org.lwjgl.opengl.GL11;
 
-final class ListenerRender extends ModuleListener<Speedmine, Render3DEvent>
-{
+final class ListenerRender extends ModuleListener<Speedmine, Render3DEvent> {
+    private AxisAlignedBB cachedBB;
+
     public ListenerRender(Speedmine module)
     {
         super(module, Render3DEvent.class);
+    }
+
+    public double Easing(double x)
+    {
+        if (x < 0.5) {
+            return 4 * x * x * x;
+        } else {
+            double f = ((2 * x) - 2);
+            return 0.5 * f * f * f + 1;
+        }
     }
 
     @Override
@@ -20,20 +31,23 @@ final class ListenerRender extends ModuleListener<Speedmine, Render3DEvent>
     {
         if (!PlayerUtil.isCreative(mc.player)
                 && module.esp.getValue() != ESPMode.None
-                && module.bb != null)
-        {
+                && module.bb != null) {
+            if (cachedBB == null || !cachedBB.equals(module.bb)) {
+                float max = Math.min(module.maxDamage, 1.0f);
+                AxisAlignedBB renderBB = module.bb;
+                if (module.growRender.getValue() && max < 1.0f) {
+                    double easedMax = Easing(max);
+                    renderBB = renderBB.grow(-0.5 + (easedMax / 2.0));
+                }
+
+                cachedBB = Interpolation.interpolateAxis(renderBB);
+            }
+
             GL11.glPushMatrix();
             GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
 
-            float max = Math.min(module.maxDamage, 1.0f);
-            AxisAlignedBB renderBB = module.bb;
-            if (module.growRender.getValue() && max < 1.0f)
-            {
-                renderBB = renderBB.grow(-0.5 + (max / 2.0));
-            }
-
-            AxisAlignedBB bb = Interpolation.interpolateAxis(renderBB);
-            module.esp.getValue().drawEsp(module, bb, max);
+            AxisAlignedBB bb = cachedBB;
+            module.esp.getValue().drawEsp(module, bb, Math.min(module.maxDamage, 1.0f));
 
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             GL11.glPopAttrib();
